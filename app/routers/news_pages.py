228d3736ef0849856f70fb29841e40a117d5e6fb
Arthur Nangai <17021137+arthurarty@users.noreply.github.com>
2026-08-02 10:32:23 +0300

    Complete image uploade logic

--- app/routers/news_pages.py
+++ app/routers/news_pages.py
@@ -4,16 +4,16 @@ import uuid
 from pathlib import Path
 
 
+UPLOAD_DIR = Path("local_files/uploads")
+UPLOAD_DIR.mkdir(exist_ok=True)
+ALLOWED_TYPES = {"image/jpeg", "image/png"}
+MAX_SIZE = 10 * 1024 * 1024  # 10MB
+
+
 router = APIRouter(
     prefix="/news-pages",
     tags=["news_pages"]
 )
-
-
-UPLOAD_DIR = Path("uploads")
-UPLOAD_DIR.mkdir(exist_ok=True)
-ALLOWED_TYPES = {"image/jpeg", "image/png"}
-MAX_SIZE = 10 * 1024 * 1024  # 10MB
 
 
 @router.post(
@@ -30,7 +30,19 @@ async def uploads_news_page(file: UploadFile):
             "Unsupported file type"
         )
     if (not file.size) or file.size > MAX_SIZE:
-        raise HTTPException(status.HTTP_400_BAD_REQUEST, "File too large")
+        raise HTTPException(status.HTTP_413_CONTENT_TOO_LARGE, "File too large")
+    if not file.filename:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, "Could not name file.")
+    ext = Path(file.filename).suffix
+    safe_name = f"{uuid.uuid4()}{ext}"
+    dest = UPLOAD_DIR / safe_name
+
+    async with aiofiles.open(dest, "wb") as out_file:
+        while True:
+            chunk = await file.read(1024 * 1024)
+            if not chunk:
+                break
+            await out_file.write(chunk)
     return {
         "message": f"Page: {file.filename} scan uploaded"
     }

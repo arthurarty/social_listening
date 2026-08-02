fe5686ca08e73669b309d4a4f43b4e118faa3131
Arthur Nangai <17021137+arthurarty@users.noreply.github.com>
2026-08-02 10:33:47 +0300

    Ft upload news page (#1)
    * Accept uploaded file
    
    * Add file validation
    
    * Complete image uploade logic

--- app/routers/news_pages.py
+++ app/routers/news_pages.py
@@ -1,4 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, status, HTTPException
+import aiofiles
+import uuid
+from pathlib import Path
+
+
+UPLOAD_DIR = Path("local_files/uploads")
+UPLOAD_DIR.mkdir(exist_ok=True)
+ALLOWED_TYPES = {"image/jpeg", "image/png"}
+MAX_SIZE = 10 * 1024 * 1024  # 10MB
 
 
 router = APIRouter(
@@ -7,11 +16,33 @@ router = APIRouter(
 )
 
 
-@router.post("/")
-async def uploads_news_page():
+@router.post(
+    "/",
+    status_code=status.HTTP_201_CREATED
+)
+async def uploads_news_page(file: UploadFile):
     """
     Uploads an image of a news paper page
     """
+    if file.content_type not in ALLOWED_TYPES:
+        raise HTTPException(
+            status.HTTP_400_BAD_REQUEST,
+            "Unsupported file type"
+        )
+    if (not file.size) or file.size > MAX_SIZE:
+        raise HTTPException(status.HTTP_413_CONTENT_TOO_LARGE, "File too large")
+    if not file.filename:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, "Could not name file.")
+    ext = Path(file.filename).suffix
+    safe_name = f"{uuid.uuid4()}{ext}"
+    dest = UPLOAD_DIR / safe_name
+
+    async with aiofiles.open(dest, "wb") as out_file:
+        while True:
+            chunk = await file.read(1024 * 1024)
+            if not chunk:
+                break
+            await out_file.write(chunk)
     return {
-        "message": "Page scan uploaded"
+        "message": f"Page: {file.filename} scan uploaded"
     }

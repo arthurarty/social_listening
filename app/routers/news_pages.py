ba12c553a1de47757cf7b93c1817d6cad5df2512
Arthur Nangai <17021137+arthurarty@users.noreply.github.com>
2026-08-02 10:27:02 +0300

    Add file validation

--- app/routers/news_pages.py
+++ app/routers/news_pages.py
@@ -1,10 +1,19 @@
-from fastapi import APIRouter, UploadFile, status
+from fastapi import APIRouter, UploadFile, status, HTTPException
+import aiofiles
+import uuid
+from pathlib import Path
 
 
 router = APIRouter(
     prefix="/news-pages",
     tags=["news_pages"]
 )
+
+
+UPLOAD_DIR = Path("uploads")
+UPLOAD_DIR.mkdir(exist_ok=True)
+ALLOWED_TYPES = {"image/jpeg", "image/png"}
+MAX_SIZE = 10 * 1024 * 1024  # 10MB
 
 
 @router.post(
@@ -15,6 +24,13 @@ async def uploads_news_page(file: UploadFile):
     """
     Uploads an image of a news paper page
     """
+    if file.content_type not in ALLOWED_TYPES:
+        raise HTTPException(
+            status.HTTP_400_BAD_REQUEST,
+            "Unsupported file type"
+        )
+    if (not file.size) or file.size > MAX_SIZE:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, "File too large")
     return {
         "message": f"Page: {file.filename} scan uploaded"
     }

02f9c2ded80c1e7a4379b5bab10b8390818fb34e
Arthur Nangai <17021137+arthurarty@users.noreply.github.com>
2026-08-04 11:16:00 +0300

    Add the session dependency

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,6 +1,8 @@
 import os
+from typing import Annotated
 
 from dotenv import load_dotenv
+from fastapi import Depends
 from sqlmodel import Session, create_engine
 
 load_dotenv()
@@ -13,3 +15,6 @@ engine = create_engine(DATABASE_URL)
 def get_session():
     with Session(engine) as session:
         yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]

d69a0382d6ab612918e5e4c21e3b6e632e763b3b
Arthur Nangai <17021137+arthurarty@users.noreply.github.com>
2026-08-06 17:40:57 +0300

    Remove commented out code

--- app/config.py
+++ app/config.py
@@ -5,7 +5,6 @@ class Settings(BaseSettings):
     app_name: str = "Social Listening"
     database_url: str
     tweet_api_io: str
-    # TWEETAPIIO
     model_config = SettingsConfigDict(env_file=".env")
 
 

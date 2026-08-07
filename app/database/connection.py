23e1e7d696d902f8369cf2860ca898ec78a4401b
Arthur Nangai <17021137+arthurarty@users.noreply.github.com>
2026-08-07 16:17:12 +0300

    Start working on news page dependency

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,6 +1,3 @@
-from typing import Annotated
-
-from fastapi import Depends
 from sqlmodel import Session, create_engine
 
 from app.config import settings
@@ -11,6 +8,3 @@ engine = create_engine(settings.database_url)
 def get_session():
     with Session(engine) as session:
         yield session
-
-
-SessionDep = Annotated[Session, Depends(get_session)]

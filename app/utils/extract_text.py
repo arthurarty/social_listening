2f31d2438e8a5628e998fb83691a8b8262ff377c
Arthur Nangai <17021137+arthurarty@users.noreply.github.com>
2026-08-09 12:12:58 +0300

    Move extract text to the utils folder

--- app/utils/extract_text.py
+++ app/utils/extract_text.py
@@ -2,16 +2,14 @@ import pytesseract
 from PIL import Image
 
 
-def main(image_path: str, output_file_name: str):
+def extract_text_from_image(image_path: str) -> str:
+    """
+    Use OCR to extract the text from an image.
+    Function uses pytesseract to read the text.
+    """
     # Load the image using Pillow
     image = Image.open(image_path)
 
     # Extract text as a single string
     extracted_text = pytesseract.image_to_string(image)
-
-    with open(output_file_name, "w") as f:
-        f.write(extracted_text)
-
-
-main("pdf_scan.png", "pdf_scan.txt")
-main("online_view.png", "online.txt")
+    return extracted_text
--- read_articles.py
+++ read_articles.py
@@ -1,5 +1,6 @@
 from app.schemas.article_schema import Article
 from app.services.instances import news_page_service
+from app.utils.extract_text import extract_text_from_image
 
 articles = [
     Article(
@@ -10,7 +11,25 @@ articles = [
 ]
 
 
-news_pages = news_page_service.get_news_pages()
-print(news_pages)
+def main():
+    """
+    Runs a pipeline to create articles from a news page
 
-news_page_service.update_news_page_articles(news_page_id=1, articles=articles)
+    1. Reads unprocessed news_pages.
+    2. Extracts the text from the images using OCR.
+    3. Creates json using an visual model and the extracted text
+    4. Updates the news page
+    """
+    news_pages = news_page_service.get_news_pages(limit=1)
+
+    for news_page in news_pages:
+        extracted_text = extract_text_from_image(news_page.file_path)
+        print(extracted_text)
+
+
+# print(news_pages)
+
+# news_page_service.update_news_page_articles(news_page_id=1, articles=articles)
+
+
+main()

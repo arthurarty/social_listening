6921443812effbd542d04db37432c29698f489b1
Arthur Nangai <17021137+arthurarty@users.noreply.github.com>
2026-08-16 11:49:25 +0300

    Add batches

--- run_senteniment_analysis.py
+++ run_senteniment_analysis.py
@@ -1,3 +1,16 @@
+import time
+
 from app.llms.sentiment import main
 
-main()
+SLEEP_TIMER = 5
+TOTAL_BATCHES = 10
+
+
+for i in range(TOTAL_BATCHES):
+    print(f"Handling bunch {i+1} of {TOTAL_BATCHES}")
+    tweet_processed = main(skip=0, no_of_tweets=5)
+    if tweet_processed == 0:
+        print("Breaking since no tweets were processed")
+        break
+    print(f"Sleeping for {SLEEP_TIMER} seconds before next bunch")
+    time.sleep(SLEEP_TIMER)
